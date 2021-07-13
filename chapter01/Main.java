bf8fc51c30ca4c6c49385165d911b2ec62e91a08
juheekang <85345123+juheekang@users.noreply.github.com>
2021-07-13 18:48:13 +0900

    Add files via upload

---
+++ chapter01/Main.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for (int N = 1; N < args.length; N++) {
+				
+		}
+	}
+}
